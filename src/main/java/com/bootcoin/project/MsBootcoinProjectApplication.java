b1a3341447404d7cd1d42786d42c9ddab4280fe1
Andres Llontop <andresllontop01@gmail.com>
2022-08-03 16:43:18 -0500

    Se crea repositorio, primer commit

---
+++ src/main/java/com/bootcoin/project/MsBootcoinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bootcoin.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootcoinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBootcoinProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcoin/project/MsBootcoinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcoin.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootcoinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
